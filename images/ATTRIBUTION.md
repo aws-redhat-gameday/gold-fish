c83d5739c0b359424db18be9cbb4adfb9da06417
Will Garcia <willgrod@amazon.com>
2023-04-18 14:41:40 +1000

    feat: add animations for end of game

---
+++ images/ATTRIBUTION.md
@@ -0,0 +1,2 @@
+<a href="https://www.vecteezy.com/free-vector/yacht">Yacht Vectors by Vecteezy</a>
+<a href="https://www.vecteezy.com/free-vector/goldfish">Goldfish Vectors by Vecteezy</a>
--- js/game.js
+++ js/game.js
@@ -24,18 +24,75 @@ const createElement = (tag, className) => {
 let firstCard = '';
 let secondCard = '';
 
+
+var animate;
+var is_start = true;
+var bodyRect;
+var elemRect;
+
+function init(imageId) {
+  imgObj = document.getElementById(imageId);
+  // imgObj.style.position= 'relative'; 
+  imgObj.style.left = '0px';
+  imgObj.style.bottom = '0px';
+  bodyRect = document.body.getBoundingClientRect();
+
+  return imgObj;
+}
+
+function end() {
+  clearTimeout(animate);
+}
+
 const checkEndGame = () => {
   const disabledCards = document.querySelectorAll('.disabled-card');
 
   if (disabledCards.length === 20) {
     clearInterval(this.loop);
-    alert(`Parabéns, ${spanPlayer.innerHTML}! Seu tempo foi de: ${timer.innerHTML}`);
+
+    document.getElementsByTagName('header')[0].style.visibility = 'hidden';
+    document.getElementsByTagName('header')[0].style.display = 'none';
+
+    document.getElementById('grid').classList.add('animate__hinge');
+    setTimeout(()=>{}, 500);
+
+    document.getElementById('logo-game').style.visibility = 'visible';
+    document.getElementById('logo-game').style.display = 'block';
+
+    document.getElementById('win').style.visibility = 'visible';
+    document.getElementById('win').style.display = 'block';
+
+    document.getElementById('goldfish').style.visibility = 'visible';
+    document.getElementById('goldfish').style.display = 'block';
+
+    document.getElementById('yacht').style.visibility = 'hidden';
+    document.getElementById('yacht').style.display = 'none';
+
+    document.getElementById('credits').style.visibility = 'visible';
+
+    const yacht = init('yacht');
+    moveRight(yacht);
+    setTimeout(end, 60000);
   }
+}
+
+const moveRight = () => {
+  imgObj.style.visibility = 'visible';
+  imgObj.style.display = 'block';
+
+  elemRect = imgObj.getBoundingClientRect();
+  var offset = elemRect.right - bodyRect.right;// check how far the image to the rightmost of boby in pixel
+  if (offset >= 0) {
+    imgObj.style.left = '0px';
+  }
+  imgObj.style.left = parseInt(imgObj.style.left) + 10 + 'px';
+  animate = setTimeout(moveRight, 20);
 }
 
 const checkCards = () => {
   const firstCharacter = firstCard.getAttribute('data-character');
   const secondCharacter = secondCard.getAttribute('data-character');
+  checkEndGame();
 
   if (firstCharacter === secondCharacter) {
 
@@ -44,7 +101,6 @@ const checkCards = () => {
 
     firstCard = '';
     secondCard = '';
-
     checkEndGame();
 
   } else {
@@ -92,7 +148,6 @@ const createCard = (character) => {
   const front = createElement('div', 'face front');
 
   const backClasseNames = ['back-1', 'back-2'];
-  console.log('face back-' + backClasseNames[randomIntFromInterval(0, 1)]);
   const back = createElement('div', 'face ' + backClasseNames[randomIntFromInterval(0, 1)]);
 
   front.style.backgroundImage = `url('../images/${character}.png')`;
